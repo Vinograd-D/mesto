f13c5b72021acdf389605192f0814cf86b4c10ad
Vinograd.D <d@97vin.ru>
2022-06-07 00:57:47 +0500

    add click for button addItem

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,23 @@ const btn = [
   document.querySelector('.profile__add-button'),
 ]
 const popupClose = document.querySelectorAll('.popup__close-button')
+const popupButtonSave = document.querySelectorAll('.popup__button')
+
 const pop = document.querySelectorAll('.popup')
-const buttonEdit = document.querySelector('.profile__edit-button')
+const form = document.querySelector('.popup__body')
+
 const name = document.querySelector('.profile__name')
 const job = document.querySelector('.profile__job')
 const nameInput = document.querySelector('#popup__name-input')
 const jobInput = document.querySelector('#popup__job-input')
-const form = document.querySelector('.popup__body')
-
+const nameOfPhotoInput = document.querySelector('#popup__name-of-photo-input')
+const linkInput= document.querySelector('#popup__link-input')
 // запись данных профиля в модальное окно и вызов
 let statusEdit = () => {
   nameInput.value = name.textContent
   jobInput.value = job.textContent
 }
-buttonEdit.onclick = statusEdit
+btn[0].onclick = statusEdit
 // buttonSaveEdit.onclick = statusSaveEdit()
 
 // перезапись данных с модального окна на html страницу
@@ -42,16 +45,16 @@ function overlay(i) {
 
 
 // закрытие попапа на кнопку ескейп
-function _handleEscClose(event) {
-  const key = event.key
+function _handleEscClose(evt) {
+  const key = evt.key
   if (key === "Escape") {
-    window.close()
+    window.close() // перестал работать после того как я ввел цикл при открытии
   }
 }
 
 // открытие попапа
 for(let i = 0; i < btn.length; i++){
-  btn[i].addEventListener("click", function(event) {
+  btn[i].addEventListener("click", function() {
     pop[i].classList.add('popup_open')
     document.addEventListener('keydown', _handleEscClose)
     overlay(i)
@@ -59,7 +62,7 @@ for(let i = 0; i < btn.length; i++){
 }
 // закрытие попапа
 for(let i = 0; i < popupClose.length; i++){
-  popupClose[i].addEventListener("click", function(event) {
+  popupClose[i].addEventListener("click", function() {
     pop[i].classList.remove('popup_open')
     document.removeEventListener('keydown', _handleEscClose)
   } )
@@ -105,8 +108,8 @@ const initCards = [
   }
 ];
 
-let containerPhotos = document.querySelector('.elements__items')
-let templateElement = document.querySelector('#elementOfCards').content
+const containerPhotos = document.querySelector('.elements__items')
+const templateElement = document.querySelector('#elementOfCards').content
 
 
 initCards.forEach(function(i) {
@@ -118,3 +121,21 @@ initCards.forEach(function(i) {
 })
 
 
+
+function addItem(name, link) {
+
+  nameOfPhotoInput.value = name
+  linkInput.value = link
+
+}
+
+popupButtonSave[1].addEventListener('click', function (evt) {
+  evt.preventDefault();
+  const templateElement = document.querySelector('#elementOfCards').content
+  let element = templateElement.querySelector('.elements__item').cloneNode(true)
+  let titleElement = element.querySelector('.elements__title')
+  let linkElement = element.querySelector('.elements__picture')
+  // element.querySelector('.elements__picture').alt = names;
+  addItem(titleElement.textContent, linkElement.src)
+  containerPhotos.appendChild(element);
+})
