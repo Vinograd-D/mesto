2f9aeb47c527808754bdbdda042645f24c09bd89
Vinograd.D <d@97vin.ru>
2022-06-08 16:55:30 +0500

    fix code

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
+'use strict'
+
+//      variables
 const btn = [
   document.querySelector('.profile__edit-button'),
   document.querySelector('.profile__add-button'),
 ]
 const popupClose = document.querySelectorAll('.popup__close-button')
-const popupButtonSave = document.querySelectorAll('.popup__button')
+
+const buttonEditProfile = document.forms.editProfile
+const buttonAddCard = document.querySelector('form[name = "addCard"]')
 
 const pop = document.querySelectorAll('.popup')
-const form = document.querySelector('.popup__content')
+const popContent = document.querySelectorAll('.popup__content')
+const changeLikeButton = document.querySelector('.elements__like-button')
 
 const name = document.querySelector('.profile__name')
 const job = document.querySelector('.profile__job')
@@ -18,64 +24,47 @@ const linkInput = document.querySelector('#popup__link-input')
 const templateElement = document.querySelector('#elementOfCards').content
 const containerPhotos = document.querySelector('.elements__items')
 
-const popupImage = pop[2].querySelector('.popup__image')
-const popupTitle = pop[2].querySelector('.popup__figcaption')
+const popWrapperImage = document.querySelector('.popup__wrapper')
+const popupImage = popWrapperImage.querySelector('.popup__image')
+const popupTitle = popWrapperImage.querySelector('.popup__figcaption')
+//---=--=-==-===-==-=--=---=--=-=-==-===-==-=--=---=--=-==-===-==-=--=---=--=-=-==-===-==-=--=---=--=-==-===-==-=--=
+//              arrow functions
 // запись данных профиля в модальное окно и вызов
-let statusEdit = () => {
+const statusEdit = () => {
   nameInput.value = name.textContent
   jobInput.value = job.textContent
 }
-btn[0].onclick = statusEdit
-// buttonSaveEdit.onclick = statusSaveEdit()
 
 // перезапись данных с модального окна на html страницу
-let statusSaveEdit = () => {
+const statusSaveEdit = () => {
   name.textContent = nameInput.value
   job.textContent = jobInput.value
 }
-
 //---=--=-==-===-==-=--=---=--=-=-==-===-==-=--=---=--=-==-===-==-=--=---=--=-=-==-===-==-=--=---=--=-==-===-==-=--=
+//        functions
+// function opened modal window
+function open(pop) {
+  pop.classList.add('popup_open')
+  escClose(pop)
+}
+//function closed modal window
+function close(pop) {
+  pop.classList.remove('popup_open')
+  escClose(pop)
+}
 
 // закрытие попапа на кнопку ескейп
-function _handleEscClose(evt) {
-  const key = evt.key
-  if (key === "Escape") {
-    window.close() // перестал работать после того как я ввел цикл при открытии
+function escClose(pop) {
+  const escape = (e) => {
+    if (e.which === 27)
+      close(pop)
+    document.removeEventListener('keydown', escape);
   }
+  document.addEventListener('keydown', escape)
 }
-
-// открытие попапа
-
-  for (let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener("click", function () {
-      pop[i].classList.add('popup_open')
-      document.addEventListener('keydown', _handleEscClose)
-      // overlay(i)
-    })
-  }
-// закрытие попапа
-  for (let i = 0; i < popupClose.length; i++) {
-    popupClose[i].addEventListener("click", function () {
-      pop[i].classList.remove('popup_open')
-      document.removeEventListener('keydown', _handleEscClose)
-    })
-  }
-
-function formSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки. О том, как это делать, расскажем позже.
-  statusSaveEdit()
-  // window.close()
-  console.log(pop[0])
-  pop[0].classList.remove('popup_open')
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 
 //---=--=-==-===-==-=--=---=--=-=-==-===-==-=--=---=--=-==-===-==-=--=---=--=-=-==-===-==-=--=---=--=-==-===-==-=--=
-
-// required photos
+// array of image objs
 const initialCards = [
   {
     name: 'Архыз',
@@ -109,20 +98,21 @@ function renderCards(nameValue, linkValue) {
   const el = templateElement.querySelector('.elements__item').cloneNode(true)
   let title = el.querySelector('.elements__title')
   let link = el.querySelector('.elements__picture')
-
+  const changeLikeButton = el.querySelector('.elements__like-button')
   title.textContent = nameValue
   link.src = linkValue
   link.alt = nameValue
 
-  el.querySelector('.elements__like-button').addEventListener('click', e => e.target.classList.toggle('elements__like-button_active')) // like
+  changeLikeButton.addEventListener('click', e =>
+    e.target.classList.toggle('elements__like-button_active')) // like
 
 //delete
 
-  link.addEventListener('click', () => {
+  link.addEventListener('click', () => { // popupImage
     popupImage.src = linkValue
     popupImage.alt = nameValue
     popupTitle.textContent = nameValue
-    pop[2].classList.add('popup_open')
+    open(pop[2])
   } )
 
 return el
@@ -132,11 +122,53 @@ initialCards.forEach((i) => {
   containerPhotos.append(renderCards(i.name, i.link));
 })
 
-
-pop[1].addEventListener('submit', function (evt) {
+//---=--=-==-===-==-=--=---=--=-=-==-===-==-=--=---=--=-==-===-==-=--=---=--=-=-==-===-==-=--=---=--=-==-===-==-=--=
+//      Buttons
+//
+buttonEditProfile.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  statusSaveEdit()
+  close(pop[0])
+});
+//
+buttonAddCard.addEventListener('submit', function (evt) {
   evt.preventDefault();
   containerPhotos.prepend(renderCards(nameOfPhotoInput.value, linkInput.value));
-  pop[1].classList.remove('popup_open')
-  pop[1].reset();
+  close(pop[1])
+  this.reset();
 });
+// call modal window "edit profile"
+btn[0].addEventListener('click', () => {
+  statusEdit()
+  open(pop[0])
 
+})
+// call modal window "add cards"
+btn[1].addEventListener('click', () => {
+  open(pop[1])
+})
+
+//---=--=-==-===-==-=--=---=--=-=-==-===-==-=--=---=--=-==-===-==-=--=---=--=-=-==-===-==-=--=---=--=-==-===-==-=--=
+// close
+popContent.forEach(container  =>
+  container.addEventListener('click', e =>
+    e.stopPropagation() // метод, который бойкотирует события
+  )
+)
+popWrapperImage.addEventListener('click', e => e.stopPropagation())
+
+
+// вызов на значок икс
+popupClose.forEach( el =>
+  el.addEventListener('click', e =>
+    close(e.target.closest('.popup')
+    )
+  )
+)
+// overlay
+pop.forEach(el =>
+  el.addEventListener('click', e =>
+    close(e.target.closest('.popup')
+    )
+  )
+)
